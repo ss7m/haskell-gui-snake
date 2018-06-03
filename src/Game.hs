46407caf83d1e935694b31c86740757388181952
sam-barr <sam.barr@me.com>
2018-06-03 14:55:05 -0500

    made code more betterer

--- src/Game.hs
+++ src/Game.hs
@@ -15,33 +15,33 @@ import Display
 import Input
 
 -- Randomly generate a location for food if food is Nothing
-genFood :: Maybe Point -> Snake -> IO Point
-genFood (Just x) snake = return $ x
-genFood Nothing  snake = do
+genFood :: State -> IO State
+genFood st@(State _ _ (Just _) _) = return $ st
+genFood st@(State s _ Nothing  _) = do
   x <- randomRIO (0, gridWidth - 1)
   y <- randomRIO (0, gridHeight - 1)
-  if (x, y) `elem` snake then
-    genFood Nothing snake
+  if (x, y) `elem` s then
+    genFood st
   else
-    return (x, y)
+    return $ setFood (Just (x, y)) st
 
+-- move snake accordingly
 doMove :: State -> State
 doMove (State s d f 0) = State (move s d) d f 0
 doMove (State s d f c) = State (eat  s d) d f $ c-1
 
-eatFood :: State -> IO State
-eatFood (State s d f c) = do
-  f' <- genFood f s
-  if s `eating` f' then
-    return $ State s d Nothing $ c+5
-  else
-    return $ State s d (Just f') c
+-- increment eat counter if snake is eating
+eatFood :: State -> State
+eatFood (State s d Nothing  c) = State s d Nothing c
+eatFood (State s d (Just f) c)
+  | s `eating` f = State s d Nothing (c+5)
+  | otherwise    = State s d (Just f) c
 
 -- Perform one step of the game
 step :: Float -> State -> IO State
 step _ state = do
   when (not $ valid gridWidth gridHeight $ getSnake state) (exitGame state)
-  eatFood $ doMove state 
+  genFood $ eatFood $ doMove state 
 
 --exit the game and print out the player's score
 exitGame :: State -> IO ()
